8624fc6c8d2ccb487e5cad803e9d87cd6ae0031d
jaeYeon <A23670@nis.co.kr>
2024-04-27 18:09:00 +0900

    [FEAT] Alternative1 추가

---
+++ src/main/java/chap1/Alternative1.java
@@ -0,0 +1,29 @@
+package chap1;
+
+import java.util.Scanner;
+
+public class Alternative1 {
+    public static void main(String args[]) {
+        System.out.println(">>>>>>>Start");
+
+        Scanner stdIn = new Scanner(System.in);
+        int n;
+        System.out.println("Alternating between '+' and '-' for n times");
+        do {
+            System.out.println("n : ");
+            n = stdIn.nextInt();
+        } while (n <= 0);
+
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                System.out.print("+");
+            } else {
+                System.out.print("-");
+            }
+
+        }
+        System.out.println("\n>>>>>>>Finish");
+
+    }
+}
