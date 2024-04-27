aed0d6635269b44556d3906b6d52e42c985c9302
jaeYeon <A23670@nis.co.kr>
2024-04-27 14:21:55 +0900

    [FIX] MAX3 class 완성

--- src/main/java/chap1/Max3.java
+++ src/main/java/chap1/Max3.java
@@ -3,11 +3,26 @@ package chap1;
 import java.util.Scanner;
 public class Max3 {
     public static void main(String[] args) {
-        System.out.println(" >>>>>> Start "  );
+        System.out.println(" >>>>>> Start ");
 
-        Scanner stdIn;
+        Scanner stdIn = new Scanner(System.in);
 
-        System.out.println(" >>>>>> Finish "  );
+        System.out.println("세 정수의 최댓값을 구합니다");
+
+        System.out.println(" a의 값 :  ");
+        int a = stdIn.nextInt();
+        System.out.println(" b의 값 :  ");
+        int b = stdIn.nextInt();
+        System.out.println(" c의 값 :  ");
+        int c = stdIn.nextInt();
+        System.out.println(" >>>>>> Finish ");
+
+
+        int max = a;
+        if (max < b) max = b;
+        if (max < c) max = c;
+        System.out.println(" The Max value is :  " + max);
     }
-
 }
+
+
