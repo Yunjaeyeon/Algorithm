57608ca8fdbf76817a2ff671abbacdd5a2c41b5b
jaeYeon <A23670@nis.co.kr>
2024-04-27 14:07:18 +0900

    [FEAT] MAX3 class 생성

---
+++ src/main/java/chap1/Max3.java
@@ -0,0 +1,13 @@
+package chap1;
+
+import java.util.Scanner;
+public class Max3 {
+    public static void main(String[] args) {
+        System.out.println(" >>>>>> Start "  );
+
+        Scanner stdIn;
+
+        System.out.println(" >>>>>> Finish "  );
+    }
+
+}
