b18dd135e1d9568b1df18a7b7e2540e158b2df98
jaeYeon <A23670@nis.co.kr>
2024-05-01 15:30:51 +0900

    [FIX] TriangleLB 메소드 추가

--- src/main/java/chap1/TriangleLB.java
+++ src/main/java/chap1/TriangleLB.java
@@ -14,12 +14,40 @@ public class TriangleLB {
             n = stdIn.nextInt();
         } while (n <= 0);
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++)
-                System.out.print("*");
-            System.out.println();
+//        // 오른쪽 위가 직각인 직각 삼각형
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= i - 1; j++)
+//                System.out.print(" ");
+//            for (int j = 1; j <= n - i + 1; j++)
+//                System.out.print('*');
+//            System.out.println();
+//        }
 
+        //오른쪽 아래가 직각인 직각 삼각형
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++)
+                System.out.print(" ");
+            for (int j = 1; j <= i; j++)
+                System.out.print('*');
+            System.out.println();
         }
         System.out.println(">>>>>>>Finish");
+
+//        //왼쪽 위가 직각인 직각 삼각형
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n - i + 1; j++)
+//                System.out.print("*");
+//            System.out.println();
+//        }
+
+
+//        //왼쪽 아래가 직각인 직각 삼각형
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= i; j++)
+//                System.out.print("*");
+//            System.out.println();
+//
+//        }
+
     }
 }
