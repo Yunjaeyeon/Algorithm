679f10b83d153eaeffdf5891b8c04dc08941ebbf
jaeYeon <A23670@nis.co.kr>
2024-05-02 08:34:13 +0900

    [FIX] TriangleLB 메소드 추가

--- src/main/java/chap1/TriangleLB.java
+++ src/main/java/chap1/TriangleLB.java
@@ -23,15 +23,14 @@ public class TriangleLB {
 //            System.out.println();
 //        }
 
-        //오른쪽 아래가 직각인 직각 삼각형
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++)
-                System.out.print(" ");
-            for (int j = 1; j <= i; j++)
-                System.out.print('*');
-            System.out.println();
-        }
-        System.out.println(">>>>>>>Finish");
+//        //오른쪽 아래가 직각인 직각 삼각형
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n - i; j++)
+//                System.out.print(" ");
+//            for (int j = 1; j <= i; j++)
+//                System.out.print('*');
+//            System.out.println();
+//        }
 
 //        //왼쪽 위가 직각인 직각 삼각형
 //        for (int i = 1; i <= n; i++) {
@@ -49,5 +48,15 @@ public class TriangleLB {
 //
 //        }
 
+//        //n단 피라미드 출력
+//            for (int i = 1; i <= n; i++) {					// i 행( i = 1, 2, ... , n )
+//                for (int j = 1; j <= n - i; j++)			// n - i 개의 ' ' 을 출력
+//                    System.out.print(' ');
+//                for (int j = 1; j <= (i-1)*2+1; j++)	// (i - 1) * 2 + 1 개의 '*' 을 출력
+//                    System.out.print('*');
+//                System.out.println();									// 줄바꿈
+//            }
+
+        System.out.println(">>>>>>>Finish");
     }
 }
