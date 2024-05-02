d82165309dd0dd06bb2a6f74487b2e60825e848d
jaeYeon <A23670@nis.co.kr>
2024-05-02 15:21:45 +0900

    [FIX] MaxOfArray 난수 생성 메서드 추가

--- src/main/java/chap2/MaxOfArray.java
+++ src/main/java/chap2/MaxOfArray.java
@@ -1,14 +1,14 @@
 package chap2;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class MaxOfArray {
-    int[] a = {120, 130, 100, 150, 140};
+    //int[] a = {120, 130, 100, 150, 140};
 
     public static int maxOf(int[] a) {
-        // 배열 a의 최대값을 구해 반환
+        // 배열 a의 최대값을 구해 반환 // 키
         int max = a[0];
-        for (int i = 0; i < a.length - 1; i++) {
+        for (int i = 0; i < a.length; i++) {
             if (a[i] > max) {
                 max = a[i];
             }
@@ -18,18 +18,23 @@ public class MaxOfArray {
 
     public static void main(String[] args) {
         System.out.println(">>>start");
-        Scanner stdIn = new Scanner(System.in);
+        Random rand = new Random();
+       // Scanner stdIn = new Scanner(System.in);
 
         System.out.println("Find the maximum value in array");
         System.out.println("Number of people :" );
 
-        int num = stdIn.nextInt();
+        // 사람 수 난수 생성
+
+        int num = 1 + rand.nextInt(10);
         int[] height = new int[num];
 
+        // 키 난수로 생성
        for(int i = 0; i < height.length; i++){
-           height[i] = stdIn.nextInt();
-           System.out.println("height[i] = " + height[i]);
+           height[i] = 100 + rand.nextInt(90);
+           System.out.println("height= " + height[i]);
        }
+
 
         System.out.println("The max value in Array is " + maxOf(height));
 
