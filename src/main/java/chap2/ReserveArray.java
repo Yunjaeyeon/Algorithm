e9c94907f75fe59be0d6f95efa01aeab25a8c2a1
jaeYeon <A23670@nis.co.kr>
2024-05-02 16:37:39 +0900

    [FIX] ReserveArray 값 입력 받기 추가

--- src/main/java/chap2/ReserveArray.java
+++ src/main/java/chap2/ReserveArray.java
@@ -1,28 +1,59 @@
 package chap2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ReserveArray {
+
+    static void swap(int[] arr, int idx1, int idx2) {
+        int t = arr[idx1]; //arr[idx1] = {2, 5, 1, 3, 9, 6, 7};
+        arr[idx1] = arr[idx2]; //{2, 5, 1} = {9,6,7}
+        arr[idx2] = t;
+    }
+
+    static void reserve(int[] arr){
+        for (int i = 0; i < arr.length /2; i++) {
+            swap(arr, i ,arr.length -1 - i);
+        }
+    }
 
 
     public static void main(String[] args) {
         System.out.println(">>>start");
+        Scanner stdIn = new Scanner(System.in);
 
-        int[] arr = {2, 5, 1, 3, 9, 6, 7};
+        System.out.print("numbers of element :");
+        int arr = stdIn.nextInt();
 
-        //reserve array
-        for (int i = 0; i < arr.length / 2; i++) {
-           // System.out.println("arr = " + arr[i]);
-            swap(arr, i, arr.length - 1 - i); // swap 메소드 호출 시 뒤집을 범위의 인덱스 전달
+        int[] x = new int[arr]; // 입력받을 요소의 수
+        //int[] arr = {2, 5, 1, 3, 9, 6, 7};
+
+        for (int i = 0; i < arr; i++) { // 요소 입력받기
+            System.out.print("x[i]:"  );
+            x[i] = stdIn.nextInt();
+
         }
 
-        for(int arrs : arr){
-            System.out.println("arrs = " + arrs);
-        }
+        reserve(x);
+        System.out.println("The array has been reserved" );
+        System.out.println(" x= " + Arrays.toString(x));
         System.out.println(">>>finish");
+
+
+
+//        //배열 뒤집기
+//        for (int i = 0; i < arr.length/2 ; i++) {
+//            swap(arr, i, arr.length - i - 1);
+//
+//        }
+//
+//        // 뒤집은 배열 출력
+//        for (int arrs : arr) {
+//            System.out.println("arrs : " + arrs);
+//        }
+
+
     }
 
-    static void swap(int[] arr, int idx1, int idx2) {
-        int t = arr[idx1]; // 6
-        arr[idx1] = arr[idx2]; // idx1에 idx2의 값을 할당
-        arr[idx2] = t; // index2에는 임시변수t(idx1)에 저장된 값을 할당
-    }
+
 }
